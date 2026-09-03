5b1b2bdaddb0a13ef20cf0a57bff28c8cc146cf4
ManishTheAce <manishgoutam2030@gmail.com>
2026-09-03 12:15:24 +0530

    print

--- call.py
+++ call.py
@@ -2,8 +2,7 @@
 Simple Command-Line Calculator
 Supports: +, -, *, /, % (modulus), ** (power)
 """
-
-print("--------------Calculator---------------------")
+print("-----------------Calculator----------------")
 
 def calculate(num1, operator, num2):
     if operator == '+':
